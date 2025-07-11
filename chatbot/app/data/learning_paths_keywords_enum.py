2c093c3bd71dffbf450b1f9baab517f8cdedbb72
Fernando Cavalcanti <fafhcavalcanti@gmail.com>
2025-07-11 16:37:18 -0300

    [Add] Chatbot keyword recognition feature

---
+++ chatbot/app/data/learning_paths_keywords_enum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+# Define knowledge paths and associated keywords/tags using an Enum
+class LearningPath(Enum):
+    CALCULO_INCENTIVO = "Cálculo do Incentivo"
+    LANCAMENTOS_INCENTIVO = "Lançamentos do Incentivo"
+    CONTROLES_SUPLEMENTARES = "Controles Suplementares"
+
+# Those keywords are used to match words used in prompt with knowledge paths
+LEARNING_PATHS_KEYWORDS = {
+    LearningPath.CALCULO_INCENTIVO.value: ["calculo", "saldo", "apuracao", "saldo devedor"],
+    LearningPath.LANCAMENTOS_INCENTIVO.value: ["lancamento", "deducao", "codigo proprio", "taxa de administracao", "taxa de adm", "taxa do feef", "feef"],
+    LearningPath.CONTROLES_SUPLEMENTARES.value: ["icms", "imposto sobre operacoes relativas a circulacao de mercadorias", "identificacao e correcao", "calculo", "diferenca", "recolhimento"],
+}
+
