212c74a1264e45a63b2a06e5449f3e908cb17334
Giulia Ferreira <giuliamarianna18@gmail.com>
2025-07-31 10:26:46 -0300

    feat: add CRUD user profile

--- back/users/views.py
+++ back/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite aos usuários serem vistos ou editados.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
